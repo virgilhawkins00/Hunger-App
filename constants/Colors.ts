17666b53c6b331a1134a47af8a600bb477e35c15
arTW0 <arthur.moreira@itec.ufpa.br>
2024-06-29 13:11:28 -0300

    new colros

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,6 +1,14 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
+const orange100 = '#EBBBA7';
+const orange200 = '#E08B76';
+const orange300 = '#EF6442';
+const orange400 = '#CE4B27';
+const offWhite = '#F0CFA9';
+const green100 = '#5E8E44';
+const green200 = '#2B502F';
+
 export default {
   light: {
     text: '#000',
